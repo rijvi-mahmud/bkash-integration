e01c63497e7ac3e5b13bfd069fea5245f83ffd12
rijvimahmudd <93908058+rijvimahmudd@users.noreply.github.com>
2024-05-13 23:45:52 +0600

    bkash-integration

---
+++ Frontend/src/Hooks/useGlobal.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
+import countryCode from "../Features/Checkout/Data/countryCode.json";
+const useGlobal = () => {
+  const [open, setOpen] = useState(false);
+  const [mbCode, setMbCode] = useState(countryCode[15]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+
+  const toggleModal = () => setOpen(!open);
+
+  const getPayment = async (body) => {
+    try {
+      const {data} = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api/bkash/createPayment`, { ...body, totalPrice });
+      window.location.href = data.bkashURL
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return {
+    toggleModal,
+    open,
+    setMbCode,
+    mbCode,
+    getPayment,
+    totalPrice,
+    setTotalPrice,
+    setOpen
+  };
+};
+export default useGlobal;
